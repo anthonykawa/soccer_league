e9cb959463bcbbf674709f64ac587f3d9232b915
Anthony Kawa <anthonyk2020@gmail.com>
2018-06-25 14:51:14 -0700

    Cleaned up code and added function to read data from CSV

--- league_builder.py
+++ league_builder.py
@@ -1,21 +1,50 @@
 
-if __name__ == "__main__":
+import csv
+
+TEAMS = ["Raptors", "Dragons", "Sharks"]
+CSV_FILE = 'soccer_players.csv'
+
+# Will take in a csv file and return a dict of the player details in a list format
+def csv_to_players(file):
+    results = []
+    with open(file) as file:
+        for row in csv.DictReader(file):
+            results.append(row)
+    return results
 
 # Take players and divide into two separate dict(). One dict() will have experienced: YES, and the other will have experienced: NO
-def parse_players(players):
-    experienced_players = []
-    inexperienced_players = []
-    for player in players:
-        if player['experienced'] == "YES":
-            experienced_players.append(player)
-        else:
-            inexperienced_players.append(player)
-    return {"experienced_players": experienced_players, "inexperienced_players": inexperienced_players}
+def create_player(team_name):
+    return {"team_name": team_name, "avg_height": 0, "players": []}
 
 
-# take in a dict() 
-#   ( ex. {"name": "fred", "experienced": True, ""parents": "Ed and Wilma"} ) 
-# and divide in 3 different teams, then return teams 
-#   (ex. {"team": "Rangers", [{"name": "fred", "experienced": True, ""parents": "Ed and Wilma"}], })
 def create_teams(players, teams):
+    while players:
+        for team in teams:
+            team['players'].append(players.pop(0))
+
+# take in players and write to file the teams and their respective players
+def players_to_file(players):
     pass
+
+def run():
+
+    teams = []
+    for team in TEAMS:
+        teams.append(create_player(team))
+
+    # get players from CSV file and assign them to players
+    players = csv_to_players(CSV_FILE)
+    # Separate the experienced players from the inexperienced players
+    exp_players = []
+    inexp_players = []
+    for player in players:
+        if player["Soccer Experience"] == "YES":
+            exp_players.append(player)
+        else:
+            inexp_players.append(player)
+    create_teams(exp_players, teams)
+    create_teams(inexp_players, teams)\
+
+
+if __name__ == "__main__":
+    run()
--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 import league_builder
-
-PLAYERS = [
-    {"name": "Frederick Gonzalez", "experienced": "YES", "guardians": "Fred and Wilma"},
-    {"name": "Dederick Gallahan", "experienced": "NO", "guardians": "Your mom"}
-]
-
-print(league_builder.parse_players(PLAYERS))
